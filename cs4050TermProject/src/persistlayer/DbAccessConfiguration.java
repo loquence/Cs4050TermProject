827985f8eafc3dbc957b38df4a2fba6e30e96ac0
loquence <ryandryv@gmail.com>
2017-10-31 12:04:15 -0400

    Add files via upload
    Updated signup.html to verify email and password with javascript

--- cs4050TermProject/src/persistlayer/DbAccessConfiguration.java
+++ cs4050TermProject/src/persistlayer/DbAccessConfiguration.java
@@ -4,7 +4,7 @@ public abstract class DbAccessConfiguration {
 
 	static final String DRIVE_NAME = "com.mysql.jdbc.Driver";
 	
-	static final String CONNECTION_URL = "jdbc:mysql://localhost:3306/bookstore";
+	static final String CONNECTION_URL = "jdbc:mysql://localhost:3306/bookstore?verifyServerCertificate=false&useSSL=true";
 	
 	static final String DB_CONNECTION_USERNAME = "root";
 	
